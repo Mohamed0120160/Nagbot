93a15bfe3d0dae21dde02d6626fb0fd5906566b7
Mohamed0120160 <142312063+Mohamed0120160@users.noreply.github.com>
2026-06-19 21:47:11 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,85 @@
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import { Button, ButtonV2,Carousel, AIRich } from './lib/MessageBuilder.js';
+
+global.Button = Button;
+global.ButtonV2 = ButtonV2;
+global.Carousel = Carousel;
+global.AIRich = AIRich;
+
+global.pairingNumber = 201281392362;
+global.owner = [
+  ['201201609786', 'R∆yGumo', true],
+['201023492122', 'kirin', true],];
+
+global.namebot = 'NagumoBot';
+global.author = 'R∆yGumo';
+global.source = 'https://chat.whatsapp.com/HGV5hvddTPIISkvFNxvoSE';
+
+global.wait = 'Loading... | جاري الانتظار';
+global.eror = 'There is an error... | وقع خطأ';
+
+global.pakasir = {
+	slug: 'kilersbotz',
+	apikey: 'bWDO2M8GcfruzXscdKNQJC3vw8Y8PV13',
+	expired: 30, //1 = 1menit. 30 = 30menit
+};
+
+global.stickpack = 'Created By';
+global.stickauth = namebot;
+
+global.multiplier = 38; // The higher, The harder levelup
+
+/*============== EMOJI ==============*/
+global.rpg = {
+	emoticon(string) {
+		string = string.toLowerCase();
+		let emot = {
+			level: '📊',
+			limit: '🎫',
+			health: '❤️',
+			stamina: '🔋',
+			exp: '✨',
+			money: '💹',
+			bank: '🏦',
+			potion: '🥤',
+			diamond: '💎',
+			common: '📦',
+			uncommon: '🛍️',
+			mythic: '🎁',
+			legendary: '🗃️',
+			superior: '💼',
+			pet: '🔖',
+			trash: '🗑',
+			armor: '🥼',
+			sword: '⚔️',
+			pickaxe: '⛏️',
+			fishingrod: '🎣',
+			wood: '🪵',
+			rock: '🪨',
+			string: '🕸️',
+			horse: '🐴',
+			cat: '🐱',
+			dog: '🐶',
+			fox: '🦊',
+			petFood: '🍖',
+			iron: '⛓️',
+			gold: '🪙',
+			emerald: '❇️',
+			upgrader: '🧰',
+		};
+		let results = Object.keys(emot)
+			.map((v) => [v, new RegExp(v, 'gi')])
+			.filter((v) => v[1].test(string));
+		if (!results.length) return '';
+		else return emot[results[0][0]];
+	},
+};
+
+let file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+	unwatchFile(file);
+	console.log(chalk.redBright("Update 'config.js'"));
+	import(`${file}?update=${Date.now()}`);
+});
